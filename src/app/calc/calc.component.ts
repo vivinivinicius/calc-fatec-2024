5e6c999da948c6d0d64ac1826d907acca4602191
Vinicius <viniciuscontator@gmail.com>
2024-02-27 17:05:50 -0300

    split added

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -9,11 +9,17 @@ export class CalcComponent {
 
   num1: number = 0;
   num2: number = 0;
+  num3: number = 0;
+  num4: number = 0;
   sum: number = 0;
 
   calc(){
   this.sum = this.num1 + this.num2;
   }
+
+  calc2(){
+    this.sum = this.num3 / this.num4;
+    }
 
 
 }
