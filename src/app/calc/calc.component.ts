0e8959a69a38aa72981f31d12936166b629990be
Vinicius <viniciuscontator@gmail.com>
2024-02-27 18:00:23 -0300

    Added multiply, divide and subtract system & buttons

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -11,14 +11,28 @@ export class CalcComponent {
   num2: number = 0;
   num3: number = 0;
   num4: number = 0;
+  num5: number = 0;
+  num6: number = 0;
+  num7: number = 0;
+  num8: number = 0;
   sum: number = 0;
+  sumdivis: number = 0;
+  summulti: number = 0;
+  sumsub: number = 0;
 
   calc(){
   this.sum = this.num1 + this.num2;
   }
 
   calc2(){
-    this.sum = this.num3 / this.num4;
+    this.sumdivis = this.num3 / this.num4;
+    }
+
+    calc3() {
+      this.summulti = this.num5 * this.num6;
+    }
+    calc4() {
+      this.sumsub = this.num7 - this.num8;
     }
 
 
